4dba57aeb2fc69591a7b093d75211bc8824dc195
penbeat <penbeat@penbeats-MacBook-Pro.local>
2024-02-07 17:26:16 -0800

    bugfix for route handling

--- app.py
+++ app.py
@@ -6,13 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', names=names)
 
-@app.route('/add_name', methods=['GET', 'POST'])
+@app.route('/add_name', methods=['POST'])
 def add_name():
     if request.method == 'POST':
         name = request.form['name']
         names.append(name)
         return redirect(url_for('index'))
-    return redirect(url_for('index'))  # Redirect GET requests back to the landing page
+    else:
+        return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
